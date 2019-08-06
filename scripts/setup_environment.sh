2c98adf2577ab90ea4e6a6e02637f98288a50d86
Andrew Evans <andrewevans0102@gmail.com>
2019-08-06 16:06:38 -0400

    resolving build script for deployment

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -9,15 +9,18 @@ curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.r
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_DEPLOYED'environment.ts'
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_DEPLOYED'environment.prod.ts'
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_DEPLOYED'cypress.json'
+curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_DEPLOYED'permissions.json'
 
 rm -rf ../src/environments/environment.ts
 rm -rf ../src/environments/environment.prod.ts
 rm -rf ../cypress.json
+rm -rf ../functions/permissions.json
 
 cp environment.ts ../src/environments/environment.ts
 cp environment.prod.ts ../src/environments/environment.prod.ts
 cp .env ../functions/.env
 cp cypress.json ../cypress.json
+cp permissions.json ../functions/permissions.json
 
 cd ..
 rm -rf 'setup_environment'
--- scripts/staged_commit.sh
+++ scripts/staged_commit.sh
@@ -9,15 +9,18 @@ curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.r
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_EMPTY'environment.ts'
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_EMPTY'environment.prod.ts'
 curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_EMPTY'cypress.json'
+curl -H "Authorization:token $RDCC_TOKEN" -H "Accept:application/vnd.github.v3.raw" -O -L $RDCC_EMPTY'permissions.json'
 
 rm -rf ../src/environments/environment.ts
 rm -rf ../src/environments/environment.prod.ts
 rm -rf ../cypress.json
+rm -rf ../functions/permissions.json
 
 cp environment.ts ../src/environments/environment.ts
 cp environment.prod.ts ../src/environments/environment.prod.ts
 cp .env ../functions/.env
 cp cypress.json ../cypress.json
+cp permissions.json ../functions/permissions.json
 
 cd ..
 rm -rf 'staged_commit'
